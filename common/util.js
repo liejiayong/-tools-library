76b4831d68344b183a92c8e233719e914ea646d7
liejiayong <809206619@qq.com>
2018-11-14 18:14:12 +0800

    update utile.getQueryString

--- common/util.js
+++ common/util.js
@@ -5,7 +5,7 @@
  * @returns {number} 返回随机数
  */
 function getRandom(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min)
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 /**
@@ -14,14 +14,14 @@ function getRandom(min, max) {
  * @returns {Blob|ArrayBuffer|Array.<T>|string|*}
  */
 export function shuffle(arr) {
-  let _arr = arr.slice()
+  let _arr = arr.slice();
   for (let i = 0; i < _arr.length; i++) {
-    let j = getRandom(0, i)
-    let n = _arr[i]
-    _arr[i] = _arr[j]
-    _arr[j] = n
+    let j = getRandom(0, i);
+    let n = _arr[i];
+    _arr[i] = _arr[j];
+    _arr[j] = n;
   }
-  return _arr
+  return _arr;
 }
 
 /**
@@ -33,24 +33,25 @@ export function shuffle(arr) {
  * @returns {Function}
  */
 export function debounce(fn, delay) {
-  let timer = null
-  return function (...args) {
-    if (timer) clearTimeout(timer)
+  let timer = null;
+  return function(...args) {
+    if (timer) clearTimeout(timer);
     timer = setTimeout(() => {
-      fn.apply(this, args)
-    }, delay)
-  }
+      fn.apply(this, args);
+    }, delay);
+  };
 }
 
 function debounce_es5(fn, delay) {
   var timer = null;
-  return function () {
-    var context = this, arg = arguments;
+  return function() {
+    var context = this,
+      arg = arguments;
     if (timer) clearTimeout(timer);
-    timer = setTimeout(function () {
+    timer = setTimeout(function() {
       fn.apply(context, arg);
     }, delay);
-  }
+  };
 }
 
 /**
@@ -62,37 +63,39 @@ function debounce_es5(fn, delay) {
  * @returns {Function}
  */
 export function throttle(fn, threshhold = 160) {
-  var timer = null
-  var start = Date.now()
-  return function (...args) {
-    var curr = Data.now()
-    if (timer) clearTimeout(timer)
+  var timer = null;
+  var start = Date.now();
+  return function(...args) {
+    var curr = Data.now();
+    if (timer) clearTimeout(timer);
     if (curr - start >= threshhold) {
-      fn.apply(this, args)
-      start = curr
+      fn.apply(this, args);
+      start = curr;
     } else {
       timer = setTimeout(() => {
-        fn.apply(this, args)
-      }, threshhold)
+        fn.apply(this, args);
+      }, threshhold);
     }
-  }
+  };
 }
 
 function throttle_es5(fn, threshhold = 160) {
   var timer = null;
   var start = Data.now();
-  return function () {
-    var context = this, arg = arguments, curr = Data.now();
+  return function() {
+    var context = this,
+      arg = arguments,
+      curr = Data.now();
     if (timer) clearTimeout(timer);
     if (curr - start >= threshhold) {
       fn.apply(context, arg);
       start = curr;
     } else {
-      timer = setTimeout(function () {
+      timer = setTimeout(function() {
         fn.apply(context, arg);
       }, threshhold);
     }
-  }
+  };
 }
 
 /**
@@ -100,10 +103,21 @@ function throttle_es5(fn, threshhold = 160) {
  * 采用扩展运算符可以识别四个字符的Unicode字符，
  * 因此使用扩展运算符结构字符串为数组，能返回实际的字符长度
  * 注：凡是涉及到操作四个字节的 Unicode 字符的函数，最好都用扩展运算符改写。
- * 
- * @param {*} str 
- * @returns {Number} 
+ *
+ * @param {*} str
+ * @returns {Number}
  */
 export function length(str) {
-  return [...str].length
+  return [...str].length;
+}
+
+/**
+ * 获取url query的指定参数
+ * @param {*} name 
+ * @returns {String}
+ */
+function getQueryString(name) {
+  var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)');
+  var r = window.location.search.substr(1).match(reg);
+  return null != c ? unescape(c[2]) : null
 }

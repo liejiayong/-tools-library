52123821fd4ce32e8bef7faba3b56e4c403abbe4
liejiayong <809206619@qq.com>
2019-03-02 17:40:41 +0800

    update : generator-css-sprite_img

---
+++ solution/generator-css-sprite_img/src/js/config.js
@@ -0,0 +1,196 @@
+var questions = [
+  {
+    index: 0,
+    title: '体大而身轻，肚内火烧心（打一物）',
+    answer: [
+      {
+        name: 'A.灯笼'
+      },
+      {
+        name: 'B.风筝'
+      },
+      {
+        name: 'C.游船'
+      }
+    ],
+    right: 0
+  },
+  {
+    index: 1,
+    title: '三令五申（打一西游人物）',
+    answer: [
+      {
+        name: 'A.唐僧'
+      },
+      {
+        name: 'B.悟空'
+      },
+      {
+        name: 'C.八戒'
+      }
+    ],
+    right: 2
+  },
+  {
+    index: 2,
+    title: '千里相逢（打一字）',
+    answer: [
+      {
+        name: 'A.俚'
+      },
+      {
+        name: 'B.重'
+      },
+      {
+        name: 'C.理'
+      }
+    ],
+    right: 1
+  },
+  {
+    index: 3,
+    title: '爱好旅行（打一成语）',
+    answer: [
+      {
+        name: 'A.百里挑一'
+      },
+      {
+        name: 'B.日行千里'
+      },
+      {
+        name: 'C.喜出望外'
+      }
+    ],
+    right: 2
+  },
+  {
+    index: 4,
+    title: '守门员（打一字）',
+    answer: [
+      {
+        name: 'A.闪'
+      },
+      {
+        name: 'B.们'
+      },
+      {
+        name: 'C.闷'
+      }
+    ],
+    right: 0
+  },
+  {
+    index: 5,
+    title: '运动会全有它（打一字）',
+    answer: [
+      {
+        name: 'A.人'
+      },
+      {
+        name: 'B.云'
+      },
+      {
+        name: 'C.力'
+      }
+    ],
+    right: 1
+  },
+  {
+    index: 6,
+    title: '一路平安（打一地名）',
+    answer: [
+      {
+        name: 'A.旅顺'
+      },
+      {
+        name: 'B.宁波'
+      },
+      {
+        name: 'C.南昌'
+      }
+    ],
+    right: 0
+  },
+  {
+    index: 7,
+    title: '一个孩子生的好，衣服穿了七八套，头上戴着红缨帽，身上装着珍珠宝。（打一植物）',
+    answer: [
+      {
+        name: 'A.玉米'
+      },
+      {
+        name: 'B.高粱'
+      },
+      {
+        name: 'C.黄瓜'
+      }
+    ],
+    right: 0
+  },
+  {
+    index: 8,
+    title: '金银铜铁（打一地名）',
+    answer: [
+      {
+        name: 'A.常州'
+      },
+      {
+        name: 'B.苏州'
+      },
+      {
+        name: 'C.无锡'
+      }
+    ],
+    right: 2
+  },
+  {
+    index: 9,
+    title: '上下一体（打一字）',
+    answer: [
+      {
+        name: 'A.卡'
+      },
+      {
+        name: 'B.尖'
+      },
+      {
+        name: 'C.赤'
+      }
+    ],
+    right: 0
+  },
+  {
+    index: 10,
+    title: '大小难辨（打一字）',
+    answer: [
+      {
+        name: 'A.一'
+      },
+      {
+        name: 'B.尖'
+      },
+      {
+        name: 'C.王'
+      }
+    ],
+    right: 1
+  },
+  {
+    index: 11,
+    title: '驼背公公，力大无穷，爱驮什么，车水马龙（打一字）',
+    answer: [
+      {
+        name: 'A.牛'
+      },
+      {
+        name: 'B.骆驼'
+      },
+      {
+        name: 'C.桥'
+      }
+    ],
+    right: 2
+  }
+];
+
+var titConfig = ['第一题', '第二题', '第三题'];

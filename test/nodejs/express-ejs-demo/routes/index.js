1afb2f55634b8eb68aac7d93ff490ed73a551952
liejiayong <809206619@qq.com>
2019-04-22 19:40:35 +0800

    update: express-ejs

--- test/nodejs/express-ejs-demo/routes/index.js
+++ test/nodejs/express-ejs-demo/routes/index.js
@@ -1,23 +1,32 @@
 module.exports = function (app) {
+  // 主页
   app.get('/', function (req, res) {
     res.render('index', { title: 'Express' })
   })
+  // 注册
   app.get('/reg', function (req, res) {
     res.render('reg', { title: '注册' })
   })
   app.post('/reg', function (req, res) {
   })
+  // 登录
   app.get('/login', function (req, res) {
     res.render('reg', { title: '登录' })
   })
   app.post('/login', function (req, res) {
   })
+  // 发布post
   app.get('/post', function (req, res) {
     res.render('reg', { title: '发表' })
   })
   app.post('/post', function (req, res) {
   })
+  // 登出
   app.get('/logout', function (req, res) {
 
+  })
+  // 用户
+  app.get('/:user', function (req, res) {
+    res.render('user', { name: req.params.user })
   })
 }

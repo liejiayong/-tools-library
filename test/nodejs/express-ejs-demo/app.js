8efde709ce2f32476b0bb50a6c4e9660f132855d
liejiayong <809206619@qq.com>
2019-04-25 18:53:56 +0800

    update config route]

--- test/nodejs/express-ejs-demo/app.js
+++ test/nodejs/express-ejs-demo/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session')
 
 var routes = require('./routes/index');
 
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session());
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- test/nodejs/express-ejs-demo/routes/index.js
+++ test/nodejs/express-ejs-demo/routes/index.js
@@ -1,32 +1,41 @@
 module.exports = function (app) {
   // 主页
   app.get('/', function (req, res) {
-    res.render('index', { title: 'Express' })
+    // res.render('index', { title: 'jy' })
+    res.redirect('/posts')
   })
-  // 注册
-  app.get('/reg', function (req, res) {
-    res.render('reg', { title: '注册' })
-  })
-  app.post('/reg', function (req, res) {
-  })
-  // 登录
-  app.get('/login', function (req, res) {
-    res.render('reg', { title: '登录' })
-  })
-  app.post('/login', function (req, res) {
-  })
-  // 发布post
-  app.get('/post', function (req, res) {
-    res.render('reg', { title: '发表' })
-  })
-  app.post('/post', function (req, res) {
-  })
-  // 登出
-  app.get('/logout', function (req, res) {
 
-  })
-  // 用户
-  app.get('/:user', function (req, res) {
-    res.render('user', { name: req.params.user })
-  })
+  app.use('/signup', require('./signup'));
+  app.use('/signin', require('./signin'));
+  app.use('/signout', require('./signout'));
+  app.use('/posts', require('./posts'));
+  app.use('/comments', require('./comments'));
+  app.use('/user', require('./user'));
+
+  // // 注册
+  // app.get('/reg', function (req, res) {
+  //   res.render('reg', { title: '注册' })
+  // })
+  // app.post('/reg', function (req, res) {
+  // })
+  // // 登录
+  // app.get('/login', function (req, res) {
+  //   res.render('reg', { title: '登录' })
+  // })
+  // app.post('/login', function (req, res) {
+  // })
+  // // 发布post
+  // app.get('/post', function (req, res) {
+  //   res.render('reg', { title: '发表' })
+  // })
+  // app.post('/post', function (req, res) {
+  // })
+  // // 登出
+  // app.get('/logout', function (req, res) {
+
+  // })
+  // // 用户
+  // app.get('/:user', function (req, res) {
+  //   res.render('user', { name: req.params.user })
+  // })
 }

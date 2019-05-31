e1c4fefdd61bbc0dd14b1439d7d0e7710c9c7f91
liejiayong <809206619@qq.com>
2019-05-31 19:08:55 +0800

    update:html:table

--- animation-tool/requestAnimateFrame.js
+++ animation-tool/requestAnimateFrame.js
@@ -3,27 +3,26 @@
  * requestAnimateFrame 兼容性
  */
 
-;(function (window) {
-    let lastTime = 0;
-    let vendors = ['webkit','moz','ms','o'];
-    for(let x =0;x<vendors.length&&!window.requestAnimationFrame ;x++){
-        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+; (function () {
+    var lastTime = 0;
+    var vendors = ['webkit', 'moz', 'ms', 'o'];
+    for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] ||
+            window[vendors[x] + 'CancelRequestAnimationFrame'];
     }
-    if(!window.requestAnimationFrame){
-        window.requestAnimationFrame = function (callback,element){
-         let currTime = Date.now();
-         let timeToCall = Math.max(0,16.7-(currTime-lastTime));
-         let id = window.setTimeout(function () {
-             callback(currTime+timeToCall)
-         },timeToCall)
+    if (!window.requestAnimationFrame) {
+        window.requestAnimationFrame = function (callback) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function () { return callback(currTime + timeToCall); }, timeToCall);
             lastTime = currTime + timeToCall;
-         return id;
-        }
+            return id;
+        };
     }
-    if(!window.cancelAnimationFrame){
+    if (!window.cancelAnimationFrame) {
         window.cancelAnimationFrame = function (id) {
             clearTimeout(id);
-        }
+        };
     }
-})(window);
+})();

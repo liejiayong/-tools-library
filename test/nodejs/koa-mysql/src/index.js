b38ba8e0c840b42c5ab62cd1e306fd13bc027fa4
liejiayong <809206619@qq.com>
2019-08-29 00:02:50 +0800

    继续折腾koa热更新

--- test/nodejs/koa-mysql/src/index.js
+++ test/nodejs/koa-mysql/src/index.js
@@ -7,32 +7,48 @@ const staticCache = require('koa-static-cache')
 const router = require('./router')
 // const koaWebpack = require('./lib/koawebpack')
 const webpack = require('webpack')
-const koaWebpack = require('koa-webpack')
+const webpackDevMiddleware = require('koa-webpack-dev-middleware')
+const webpackHotMiddleware = require('koa-webpack-hot-middleware')
 const config = require('../webpack.config')
 const { HTTP_SERVER_PORT } = require('./config/default')
 const staticPath = '/cachefile'
 
 const app = new Koa()
 
-async function start (){
-  // 热更新
-  // koaWebpack(app)
-  // const compiler = webpack(config)
-  // const middleware = await koaWebpack({ compiler })
-  // app.use(middleware)
+// 配置静态资源加载
+// app.use(koaStatic(path.join(__dirname, staticPath)))
+app.use(staticCache(path.join(__dirname, staticPath), { dynamic: true }, { maxAge: 365 * 24 * 60 * 60 }))
 
-  // 配置静态资源加载
-  // app.use(koaStatic(path.join(__dirname, staticPath)))
-  app.use(staticCache(path.join(__dirname, staticPath), { dynamic: true }, { maxAge: 365 * 24 * 60 * 60 }))
-  
-  // 配置服务器路由请求
-  app.use(bodyParser({ formLimit: '1mb' }))
-  
-  // 路由
-  router(app)
-  
-  http.createServer(app.callback()).listen(HTTP_SERVER_PORT)
-  console.log(`http server listening on port ${HTTP_SERVER_PORT}`)
-}
+// 配置服务器路由请求
+app.use(bodyParser({ formLimit: '1mb' }))
 
-start()
+// 路由
+router(app)
+
+// 热更新
+// koaWebpack(app)
+// const compiler = webpack(config)
+// app.use(webpackDevMiddleware(compiler, {
+//     noInfo: true
+//     // watchOptions: {
+//     //     ignored: /node_modules/,
+//     // },
+//     // reload: true,
+//     // publicPath: config.output.publicPath,
+//     // stats: {
+//     //     colors: true
+//     // }
+// }))
+// app.use(webpackHotMiddleware(compiler))
+
+http.createServer(app.callback()).listen(HTTP_SERVER_PORT)
+console.log(`http server listening on port ${HTTP_SERVER_PORT}`)
+
+const compiler = webpack(config)
+
+const wdm = webpackDevMiddleware(compiler, {
+    noInfo: true,
+    //publicPath: config.output.publicPath
+})
+app.use(wdm)
+app.use(webpackHotMiddleware(compiler))
--- test/nodejs/koa-mysql/webpack.config.js
+++ test/nodejs/koa-mysql/webpack.config.js
@@ -1,16 +1,25 @@
 const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
-  entry: {
-    main: ['./src/index.js']
-  },
-  mode: 'development',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'js/[name].js',
-    publicPath: '/'
-  },
-  resolve: {
-    extensions: ['.js']
-  }
+    entry: {
+        main: [
+            'webpack-hot-middleware/client?noInfo=true&reload=true', // 生产环境的入口建议把这个去掉
+            './src/index.js'
+        ]
+    },
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].js',
+        publicPath: '/'
+    },
+    resolve: {
+        extensions: ['.js']
+    },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin()
+    ]
 }

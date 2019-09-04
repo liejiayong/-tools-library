3141b779b3b141e007130e0fa5906a46799c7c42
liejiayong <809206619@qq.com>
2019-09-04 11:55:36 +0800

    backup: docs and demo

--- test/nodejs/koa-mysql/src/router/index.js
+++ test/nodejs/koa-mysql/src/router/index.js
@@ -1,4 +1,7 @@
 module.exports = function (app) {
     // 注册
+    app.use(require('koa-router')().get('/', ctx => {
+        ctx.body = 'hello world'
+    }).routes())
     app.use(require('./signup').routes())
 }

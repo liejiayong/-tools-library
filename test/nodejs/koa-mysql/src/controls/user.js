db33f892b26339172c7aa57a032b8f2a8b47b6a7
liejiayong <809206619@qq.com>
2019-09-10 19:23:15 +0800

    update:

--- test/nodejs/koa-mysql/src/controls/user.js
+++ test/nodejs/koa-mysql/src/controls/user.js
@@ -3,7 +3,28 @@ const { findDataByUId } = require('../models/user')
 exports.postUserInfo = async ctx => {
     try {
         let { id } = ctx.request.body
-        console.log(ctx.query)
+        await findDataByUId(id).then(res => {
+            if (res.length) {
+                ctx.body = {
+                    code: 1,
+                    message: '获取用户信息操作成功',
+                    data: res[0]
+                }
+                console.log('获取用户信息操作成功', ctx.session)
+            } else {
+                ctx.body = {
+                    code: 20001,
+                    msg: '用户名不存在'
+                }
+                console.log('用户名不存在!')
+            }
+        })
+    } catch {}
+}
+
+exports.getUserInfo = async ctx => {
+    try {
+        let { id } = ctx.query
         await findDataByUId(id).then(res => {
             if (res.length) {
                 ctx.body = {

89d04224d367004b830391cd73d8025c12251115
liejiayong <809206619@qq.com>
2020-06-13 16:04:50 +0800

    js-drag-wall@beta-1:拖拽插件\n兼容ie10及以上现代浏览器。支持监听、禁用、随机打乱等功能\n首发测试版

--- plugin/js-drag-wall/src/drag.js
+++ plugin/js-drag-wall/src/drag.js
@@ -1,121 +1,50 @@
-// import { preStyle, getPixelRatio } from './util'
+'use strict'
+
+// import domUtil from './dom'
 import './simulateTouch.js'
-  ; ('use strict')
 
 /*
- * Description: 
- * version: 
+ * name: 拖拽插件
+ * Description: 兼容ie10及以上现代浏览器。支持监听、禁用、随机打乱等功能
+ * version: 0.0.2
  * Author: liejiayong(809206619@qq.com)
- * Date: 2020-04-10 16:29:08
+ * Date: 2020-06-13 16:29:08
  * LastEditors: liejiayong(809206619@qq.com)
- * LastEditTime: 2020-06-10 17:58:11
+ * LastEditTime: 2020-06-13 16:02:50
  */
-/* control class */
-function hasClass(el, cls) {
-  let reg = new RegExp(`(^|\\s)${cls}(\\s|$)`)
-  return reg.test(el.className)
-}
-
-function addClass(el, cls) {
-  if (hasClass(el, cls)) {
-    return
-  }
-  let newCls = el.className.split(' ')
-  newCls.push(cls)
-  el.className = newCls.join(' ')
-}
-
-function removeClass(el, cls) {
-  if (!hasClass(el, cls)) {
-    return
-  }
-  let reg = new RegExp(`(^|\\s)${cls}(\\s|$)`)
-  el.className = el.className.replace(reg, '')
-}
-
-/* control cssText style */
-function getStyle(ele, style) {
-  let ret;
-  if (window.getComputedStyle) {
-    ret = window.getComputedStyle(ele, null).getPropertyValue(style);
-  } else {
-    if (style === 'opacity') {
-      var filter = null;
-      // 早期的 IE 中要设置透明度有两个方法：
-      // 1、alpha(opacity=0)
-      // 2、filter:progid:DXImageTransform.Microsoft.gradient( GradientType= 0 , startColorstr = ‘#ccccc’, endColorstr = ‘#ddddd’ );
-      // 利用正则匹配
-      filter = ele.style.filter.match(/progid:DXImageTransform.Microsoft.Alpha\(.?opacity=(.*).?\)/i) || ele.style.filter.match(/alpha\(opacity=(.*)\)/i);
-      if (filter) {
-        var value = parseFloat(filter);
-        if (!isNaN(value)) {
-          // 转化为标准结果
-          ret = value ? value / 100 : 0;
-        }
-      }
-      // 透明度的值默认返回 1
-      ret = 1;
-    } else if (style === 'float') {
-      ret = ele.currentStyle.getAttribute('styleFloat');
-    } else if ((style === 'width' || style === 'height') && (ele.currentStyle[style] === 'auto')) {
-      // 取高宽使用 getBoundingClientRect
-      let clientRect = ele.getBoundingClientRect();
-      ret = (style === 'width' ? clientRect.right - clientRect.left : clientRect.bottom - clientRect.top) + 'px';
-    }
-    // 其他样式，无需特殊处理
-    ret = ele.currentStyle.getAttribute(style);
-  }
-  return ret;
+/**
+ * 获取随机整数
+ * @param {*} min 最小值
+ * @param {*} max 最大值
+ */
+function getRandom(min, max) {
+  return min + ~~(Math.random() * (max - min + 1))
 }
 
 /**
- * 添加css前缀
- * @param {*} style 
+ * 将对象转化为cssText字符串
+ * @param {*} params 
  */
-function preStyle(style) {
-  var el = document.createElement('div');
-
-  var vendor = (function () {
-    var transformName = {
-      webkit: 'webkitTransform',
-      Moz: 'MozTransform',
-      O: 'OTransform',
-      ms: 'msTransform',
-      standard: 'transform'
-    };
-    for (var key in transformName) {
-      if (el[key] !== 'undefined') {
-        return key;
-      }
-    }
-    return false;
-  })();
-
-  if (vendor === false) {
-    return false;
+function oToCssText(params = {}) {
+  let ret = ''
+  for (let key of params) {
+    ret += `${key}:${params[key]};`
   }
 
-  if (vendor === 'standard') {
-    return style;
-  }
-
-  return vendor + style.charAt(0).toUpperCase() + style.substr(1);
-}
-
-function transofrmFn(t) {
-  return ';-webkit-transform:' + t
-    + ';-moz-transform:' + t
-    + ';-o-transform:' + t
-    + ';-ms-transform:' + t
-    + ';transform:' + t
+  return ret
 }
 
 const DEFAULT_CONFIG = {
   parentsCls: '.jy-wall-drag',
-  childCls: '.item',
-  onReady: () => { },
+  child: {
+    cls: '.item',
+    width: 0,
+    height: 0
+  },
+  gutter: 20,
+  onMoveBefore: () => { },
   onMove: () => { },
-  onSuccess: () => { }
+  onMoveEnd: () => { }
 }
 
 class Drag {
@@ -123,8 +52,16 @@ class Drag {
     this.DEFAULT_CONFIG = DEFAULT_CONFIG
     this.options = Object.assign({}, DEFAULT_CONFIG, options)
 
+    // 元素
+    this.$parent = null // 父元素
+    this.$children = null // 拖拽元素集合
+    this.parent = null // 父元素position
+    this.target = null // 当前拖拽元素position
+    // this.diffGutter = null // 父元素与拖拽元素可活动空隙
+
+    // 状态
     this.istap = false // 按下
-    this.isplay = false // 游戏状态。true:可拖拽；false：不可拖拽
+    this.isplay = true // 游戏状态。true:可拖拽；false：不可拖拽
 
     this._initial()
   }
@@ -155,133 +92,203 @@ class Drag {
     this.isplay = false
     this.istap = false
   }
-  getOffset(clientX, clientY) {
-    const { top, left, width, height } = this.childrenPos[this.current]
-    // console.log(this.childrenPos[this.current])
+  /**
+   * 打乱布局
+   * 预设想越靠近中央越定置显示
+   */
+  random() {
+    const $parent = this.$parent, $children = this.$children, childLen = $children.length
 
-    const x = clientX - left - width / 2
-      , y = clientY - top - height / 2
-    // top = clientY -  
+    if ($children && childLen < 1) {
+      return
+    }
 
+    const { gutter, child } = this.options
+      , { width: pw, height: ph } = $parent.getBoundingClientRect()
+      , childBounding = $children[0].getBoundingClientRect()
+      , cw = child.width || childBounding.width
+      , ch = child.height || childBounding.height
+
+    // 预设5个中央显示postion:中央、左上、坐下、右上、右下
+    const rulePos = []
+    // 初步设想
+    // let temWL = pw / 2 - cw,
+    //   temWR = pw / 2,
+    //   temHT = ph / 2 - ch,
+    //   temHB = ph / 2
+    // 二次设想
+    let temWL = (pw - cw) / 2 - cw * 9 / 10 + getRandom(-20, 20)
+      , temWR = (pw - cw) / 2 + cw * 1.2 + getRandom(-20, 20)
+      , temHT = (ph - ch) / 2 - ch * 9 / 10 + getRandom(-20, 20)
+      , temHB = (ph - ch) / 2 + ch * 1.3 + getRandom(-20, 20)
+    if (temWL < 0) temWL = 0
+    if (temWR > pw - cw) temWR = pw - cw
+    if (temHT < 0) temHT = 0
+    if (temHB > ph - ch) temHB = ph - ch
+    rulePos.push({ left: temWL, top: temHT })
+    rulePos.push({ left: temWL, top: temHB })
+    rulePos.push({ left: temWR, top: temHT })
+    rulePos.push({ left: temWR, top: temHB })
+    rulePos.push({ left: (pw - cw) / 2, top: (ph - ch) / 2 }) // 中央
+
+    // random position
+    const saftPos = { top: 0, left: 0, bottom: ph - ch, right: pw - cw }
+
+    // test position
+    console.log($children, saftPos, rulePos)
+    if (childLen == 1) {
+      $children[0].style.top = `${rulePos[0].top}px`
+      $children[0].style.left = `${rulePos[0].left}px`
+    } else if (childLen <= 5) {
+      $children.slice().forEach(function (child, index) {
+        child.style.top = `${rulePos[index].top}px`
+        child.style.left = `${rulePos[index].left}px`
+      })
+    } else {
+      var diffLen = childLen - 5
+      $children.slice().forEach(function (child, index) {
+        if (index < diffLen) {
+          child.style.top = `${getRandom(saftPos.top, saftPos.bottom)}px`
+          child.style.left = `${getRandom(saftPos.left, saftPos.right)}px`
+        } else {
+          child.style.top = `${rulePos[index - diffLen].top}px`
+          child.style.left = `${rulePos[index - diffLen].left}px`
+        }
+      })
+    }
+
+
+    console.log(pw, ph, gutter, cw, ch)
+  }
+  /**
+   * 获取当前拖拽元素相对父元素的x、y轴定位
+   * @param {*} position 手势坐标信息，包含clientX,clientY,pageX,pageY,screenX,screenY...
+   * 
+   */
+  getOffset(position) {
+    const { top: pt, left: pl, right: pr, bottom: pb, width: pw, height: ph } = this.parent || this.$parent.getBoundingClientRect(),
+      { top: ct, left: cl, right: cr, bottom: cb, width: cw, height: ch } = this.$target.getBoundingClientRect(),
+      { clientX, clientY, pageX, pageY } = position,
+      curX = clientX || pageX,
+      curY = clientY || pageY
+
+    // 如果鼠标超出父元素范围则返回
+    if (pt > curX || pb < curX || pl > curY || pr < curY) {
+      return false;
+    }
+
+    let x = (curX - pl) - (cw / 2),
+      y = (curY - pt) - (ch / 2)
+
+    if (x < 0) {
+      x = 0
+    }
+    if (x > pw - cw) {
+      x = pw - cw
+    }
+    if (y < 0) {
+      y = 0
+    }
+    if (y > ph - ch) {
+      y = ph - ch
+    }
+    // console.log('test:', this.$target, x, y)
     return {
       x: parseInt(x)
       , y: parseInt(y)
-      // top: 
     }
   }
-  _precisePos(child, index, siblings) {
-    const { gutter } = this.options,
-      { x, y, width, height, left, right, top, bottom } = child.getBoundingClientRect()
-    return {
-      x: parseInt(x),
-      y: parseInt(y),
-      xEnd: parseInt(x + width),
-      yEnd: parseInt(y + height),
-      width: parseInt(width),
-      height: parseInt(height),
-      left: parseInt(left),
-      right: parseInt(right),
-      top: parseInt(top),
-      bottom: parseInt(bottom),
-      gxEnd: parseInt(right - gutter),
-      gyEnd: parseInt(bottom - gutter),
-      gleft: parseInt(left + gutter),
-      gright: parseInt(right - gutter),
-      gtop: parseInt(top + gutter),
-      gbottom: parseInt(bottom - gutter),
-      el: child,
-      index: index,
-      siblings: siblings,
-      isContain: false,
-      containZone: null
-    }
-  }
-  _getPosition(children) {
-    if (!Array.isArray(children)) {
-      return [this._precisePos(children, 0, [])]
-    }
 
-    const origins = children.map((child, index, siblings) => {
-      return this._precisePos(child, index, siblings)
-    })
-    // console.log('bottle position: ')
-    // console.table(origins)
-    return origins
-  }
   _evStart(e) {
-    // console.log('start', this.isplay, e)
-    if (!this.isplay) return;
+    if (!this.isplay) {
+      return
+    }
+    this.istap = true
 
-    const touches = e.touches[0]
-    let { clientX, clientY } = touches,
-      childrenPos = this.childrenPos, current = -1
+    const { target } = e
+    this.$target = target
+    this.target = target.getBoundingClientRect()
+    this.parent = this.$parent.getBoundingClientRect()
 
-    childrenPos.forEach(({ top, left, xEnd, yEnd }, index) => {
-      if (clientY > top && clientY < yEnd && clientX > left && clientX < xEnd) {
-        current = index
+    this.$children.forEach(function (child) {
+      if (target == child) {
+        child.style.zIndex = 1
+        child.style.cursor = 'move'
+      } else {
+        child.style.zIndex = 0
+        child.style.cursor = ''
       }
     })
 
-    if (~current) {
-      this.childrenPos[current]['el'].style.zIndex = 10
-      this.childrenPos[current]['el'].style.cursor = 'grab'
-      this.current = current
-      this.istap = true
-    } else {
-      this.current = -1
-      this.istap = false
-    }
+    this.options.onMoveBefore(this.$target, this)
 
-    console.log('111', this.current, this.istap)
+    console.log('start target', this.$target.style.cssText)
   }
   _evMove(e) {
-    console.log('2222222')
-    if (!this.isplay) return
-    if (!this.istap) return
+    e.preventDefault()
+    if (!this.isplay) {
+      return
+    }
+    if (!this.istap) {
+      return
+    }
 
-    console.log('223333333333')
-    const touches = e.touches[0]
-    this.zoneContain(touches)
+    const { targetTouches } = e
+    const curPos = this.getOffset(targetTouches[0])
+    // 只有移动手势在父元素内移动才触发
+    if (curPos) {
+      const { x, y } = curPos
+      this.$target.style.left = `${x}px`
+      this.$target.style.top = `${y}px`
+      this.options.onMove(this.$target, this)
+    }
+
   }
   _evEnd(e) {
-    if (!this.isplay) return;
-
-    const curChild = this.childrenPos[this.current]
-
-    if (curChild && curChild.isContain) {
-      this.options.onSuccess(curChild.el, curChild.containZone, curChild)
-    } else {
-      // console.log(curChild)
-      if (curChild && curChild.el) {
-        const transform = preStyle('transform')
-        curChild.el.style[transform] = 'translate(0, 0)'
-        curChild.el.style.zIndex = null
-        curChild.el.style.cursor = null
-        curChild.isContain = false
-        curChild.containZone = null
-      }
+    if (!this.isplay) {
+      return
     }
-    // console.log('curChild', curChild)
-    this.current = -1
+    console.log(this.target)
+    this.$target.style.cursor = ''
     this.istap = false
+    this.$target = null
 
-    // console.log(e.type, 'end', this.istap, curChild)
+    this.options.onMoveEnd(this)
   }
   _evDoc() {
-    var doc = document.body || document
-    document.addEventListener('touchstart', this._evStart.bind(this), true)
-    document.addEventListener('touchmove', this._evMove.bind(this), true)
-    document.addEventListener('touchend', this._evEnd.bind(this), true)
+    // var doc = document.body || document
+    const doc = this.$parent
+    doc.removeEventListener('touchstart', this._evStart.bind(this), true)
+    doc.removeEventListener('touchmove', this._evMove.bind(this), true)
+    doc.removeEventListener('touchend', this._evEnd.bind(this), true)
+    doc.removeEventListener('touchcancel', this._evEnd.bind(this), true)
+    doc.addEventListener('touchstart', this._evStart.bind(this), true)
+    doc.addEventListener('touchmove', this._evMove.bind(this), true)
+    doc.addEventListener('touchend', this._evEnd.bind(this), true)
+    doc.addEventListener('touchcancel', this._evEnd.bind(this), true)
   }
   _event() {
     this._evDoc()
   }
   _initDOM() {
-    const { parentsCls, childCls } = this.options
+    const { parentsCls, child } = this.options
     const parent = document.querySelector(parentsCls)
-    const children = document.querySelectorAll(childCls)
-    this.parent = parent
-    this.children = children
+    const children = Array.from(document.querySelectorAll(child.cls))
+    this.$parent = parent
+    this.$children = children
+
+    parent.style.cssText = `
+        position: relative;
+        overflow: hide;
+        z - index: 1;
+        `
+    children.forEach(function (child) {
+      child.style.cssText = `
+        position: absolute;
+        `
+    })
+
+
   }
   _initial() {
     this._initDOM()

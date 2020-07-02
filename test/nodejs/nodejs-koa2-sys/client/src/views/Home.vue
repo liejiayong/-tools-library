f7b964453e1bfb3c1d0d3a74ed2823f06436fa79
liejiayong <809206619@qq.com>
2020-07-02 18:44:11 +0800

    kkoa-sys:add：client

---
+++ test/nodejs/nodejs-koa2-sys/client/src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png" />
+		<HelloWorld msg="Welcome to Your Vue.js App" />
+		<!-- <button @click="increment">Count is: {{ state.count }}, double is: {{ state.double }}</button> -->
+	</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { reactive, computed } from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+	// name: 'Home',
+	// components: {
+	//   HelloWorld
+	// }
+	setup() {
+		const state = reactive({
+			count: 0,
+			double: computed(() => state.count * 2)
+		})
+		function increment() {
+			state.count++
+		}
+		return {
+			state,
+			increment,
+			HelloWorld
+		}
+	}
+}
+</script>
